f64aabf293412b326602f679fe2a46beadc5879d
rpyoungdale <youngdale95@gmail.com>
2018-08-24 18:28:04 -0500

    trying to connect with db

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask, request, redirect, url_for, render_template
+from flask_mysqldb import MySQL
 from flask_modus import Modus
 from student import Student
-
+import yaml
 
 app = Flask(__name__)
+
+db = yaml.load(open('db.yaml'))
+app.config['MYSQL_HOST'] = db['mysql_host']
+app.config['MYSQL_USER'] = db['mysql_user']
+app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DB'] = db['mysql_db']
+
+mysql = MySQL(app)
+
 modus = Modus(app) # method overriding.....gives you the ability to override the default method i.e post => patch
 
-students = []
+# students = []
 
 def find_student(id):
     return [student for student in students if student.id == id][0] #list comprehension
@@ -18,10 +28,21 @@ def root(): # will redirect you to whatever the url is for a function called ind
 @app.route('/students', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        new_student = Student(request.form['first_name'], request.form['last_name'])
-        students.append(new_student)
+        # new_student = Student(request.form['first_name'], request.form['last_name'])
+        # students.append(new_student)
+        cur = mysql.connection.cursor()
+        cur.execute('INSERT INTO students(first_name, last_name) VALUES(%s, %s)', (request.form['first_name'], request.form['last_name']))
+        mysql.connection.commit()
+        cur.close()
         return redirect(url_for('index'))
-    return render_template('index.html', students=students) # first 'students' refers to a var in index.html
+    if request.method == 'GET':
+        cur = mysql.connection.cursor()
+        resultValue = cur.execute('SELECT * FROM students')
+        if resultValue > 0:
+            studentDetails = cur.fetchall()
+            return render_template('index.html', students=studentDetails)
+    return render_template('index.html')
+     # first 'students' refers to a var in index.html
 
 @app.route('/students/new')
 def new():
